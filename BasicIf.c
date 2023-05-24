f4605143701f7448d324198e3ac8f9c2b55b93f6
Faith-vaati <vaati.faith79@gmail.com>
2023-05-24 05:04:24 -0700

    comparison

---
+++ BasicIf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	if (2==2)
+	{
+		printf("%d\n", 1);
+		return 0;
+	}
+}
+
---
+++ IfElse.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	if ( 2==1 ) {
+		printf("%d\n", 1);
+    /* Execute these statements if TRUE */
+		}
+	else {
+		printf("%d\n", 0);
+    /* Execute these statements if FALSE */
+	}
+}
